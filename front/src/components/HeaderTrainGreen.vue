b45f42a4abdce1f30fadd752665870a9367217a0
julienmarion <jmarion@getmeelo.com>
2022-10-12 11:35:38 +0200

    feat: header done

--- front/src/components/HeaderTrainGreen.vue
+++ front/src/components/HeaderTrainGreen.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="header">
-    <div class="header-logo">
-      <img src="../../public/img/logo.png" alt="Logo Train Green"/>
-    </div>
-    <div class="header-title">
-      <span>Train Green</span>
-    </div>
-  </div>
-  <div class="header-menu">
-    <ul class="header-menu-items">
-      <li v-for="item in menuItems" v-bind:key="item">{{ item }}</li>
-    </ul>
-  </div>
+  <b-row class="header">
+    <b-row class="header-title">
+      <img class="header-title-logo" src="../../public/img/logo.png" alt="Logo Train Green"/>
+      <span class="header-title-label">Train Green</span>
+    </b-row>
+    <b-row>
+      <b-button-group class="header-menu-items">
+        <b-button variant="success" v-for="item in menuItems" v-bind:key="item">{{ item }}</b-button>
+      </b-button-group>
+    </b-row>
+  </b-row>
 </template>
 
 <script lang="ts">
@@ -29,33 +27,28 @@ export default defineComponent({
 .header {
   position: sticky;
   top: 0;
-  width: 100%;
-  height: fit-content;
-  display: flex;
+  border: 0px solid;
   box-shadow: 1px 0px 10px grey;
   z-index: 999;
-  align-items: center;
-  justify-content: center;
-  align-content: center;
-
-  &-logo {
-    img {
-      height: 60px;
-    }
-  }
+  padding: 5px;
 
   &-title {
+    display: flow-root;
+    margin-bottom: 1rem;
+
+    &-logo {
+      width: 100px;
+    }
+
+    &-label {
+      font-size: 30px;
+    }
   }
 
   &-menu {
     &-items {
-      display: flex;
-      list-style-type: none;
-      justify-content: center;
-
-      li {
-        padding: 10px;
-      }
+      width: 50%;
+      margin: auto;
     }
   }
 }
