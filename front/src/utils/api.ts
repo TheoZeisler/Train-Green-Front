f103344ef4eabe70074fc09ee33dc952b48fffbd
Juulieen22 <jmarion@getmeelo.com>
2022-10-14 14:17:47 +0200

    feat: axios added (#4)

---
+++ front/src/utils/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+    timeout: 30000,
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+});
